9e4386842fc776a9e582ecaf6d8cc7e446c083e6
TENCENT\haiqingsun <haiqingsun@tencent.com>
2021-03-03 14:59:01 +0800

    Storybook deployment with GitHub action

---
+++ storybook-static/precache-manifest.17f65dceae614d3b877435d6c591ed3e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acf0221243ace64417a0d8bb079835d6",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.161b155be7f48283dab5.bundle.js"
+  },
+  {
+    "url": "runtime~main.161b155be7f48283dab5.bundle.js"
+  },
+  {
+    "url": "vendors~main.161b155be7f48283dab5.bundle.js"
+  },
+  {
+    "url": "vendors~main.161b155be7f48283dab5.bundle.js.LICENSE.txt"
+  }
+]);
